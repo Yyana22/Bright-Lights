deef1e48ab742f1b740cf036be91125a0bf02b1d
Yana <sharik.spartak2@mail.ru>
2022-03-15 22:51:28 +0300

    add content in main

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let card1 = document.getElementById('1');
+let card2 = document.getElementById('2');
+let card3 = document.getElementById('3');
+let card4 = document.getElementById('4');
+let card5 = document.getElementById('5');
+let card6 = document.getElementById('6');
